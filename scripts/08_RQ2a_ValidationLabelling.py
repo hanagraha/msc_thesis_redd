c0a67095f82c424496b53f7648160cdbd340bf7e
Hannah Graham <hannahikkai.graham@wur.nl>
2024-10-25 15:43:25 +0200

    Created timesync-like code for planet

---
+++ scripts/08_RQ2a_ValidationLabelling.py
@@ -0,0 +1,249 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 25 10:23:05 2024
+
+@author: hanna
+"""
+
+############################################################################
+
+
+# IMPORT PACKAGES
+
+
+############################################################################
+
+import os
+import glob
+import rasterio
+import numpy as np
+import geopandas as gpd
+from rasterio.features import geometry_mask
+from shapely.geometry import Point
+import matplotlib.pyplot as plt
+from rasterio.plot import show
+from shapely.geometry import box
+from rasterio.mask import mask
+from rasterio.transform import from_origin
+from matplotlib.patches import Rectangle
+
+
+
+############################################################################
+
+
+# SET UP DIRECTORY AND NODATA
+
+
+############################################################################
+# Check current working directory
+print("Current Working Directory:", os.getcwd())
+
+# Change to a new directory (ADAPT THIS!!!)
+os.chdir("C:\\Users\\hanna\\Documents\\WUR MSc\\MSc Thesis\\redd-thesis")
+
+# Verify the working directory has been changed
+print("New Working Directory:", os.getcwd())
+
+# Set nodata value
+nodata_val = 255
+
+# Set output directory
+out_dir = os.path.join(os.getcwd(), 'data', 'intermediate')
+
+# Set year range
+years = range(2013, 2024)
+
+
+
+############################################################################
+
+
+# IMPORT AND READ DATA
+
+
+############################################################################
+# Define function to read list of paths
+def read_files(pathlist):
+    
+    # Create empty list to hold arrays
+    arrlist = []
+    
+    # Iterate over each filepath
+    for path in pathlist:
+        
+        # Read file
+        with rasterio.open(path) as rast:
+            data = rast.read()
+            profile = rast.profile
+            transform = rast.transform
+            
+        # Add array to list
+        arrlist.append(data)
+            
+    return arrlist, profile, transform
+
+# Define file paths for validation rasters
+planet_paths = [f"data/validation/HighRes_{year}.tif" for year in years]
+
+# Read validation rasters
+planet_arrs, profile, transform = read_files(planet_paths)
+
+# Read validation points
+valpoints = gpd.read_file("data/validation/validation_points_geometry.shp")
+
+# Convert shape (3, 16870, 20342) to (16870, 20342, 3) for plt.imshow()
+planet_rgb_arrs = [arr.transpose(1, 2, 0) for arr in planet_arrs]
+
+
+
+############################################################################
+
+
+# FORMAT ARRAYS TO RGB STACKS
+
+
+############################################################################
+# Define function to create frames for each point
+def point_frame(point_gdf, framesize):
+    
+    # Create empty list to store frames
+    bbox_list = []
+    
+    # Iterate over each point
+    for i in range(len(valpoints)):
+        
+        # Extract point geometry
+        geom = valpoints.geometry[i]
+        
+        # Extract pixel bounds
+        minx, miny, maxx, maxy = geom.bounds
+        
+        # Create a rectangular bounding box
+        bbox = box(minx - framesize / 2, miny - framesize / 2, 
+                    maxx + framesize / 2, maxy + framesize / 2)
+        
+        bbox_list.append(bbox)
+    
+    # Copy validation points gdf
+    bbox_gdf = point_gdf.copy()
+    
+    # Add bbox infomration to gdf
+    bbox_gdf['frame'] = bbox_list
+    
+    # Make sure crs stays the same
+    bbox_gdf.crs = point_gdf.crs
+    
+    return bbox_gdf
+
+# Define function to clip validation data to each frame
+def clip_raster(raster_pathlist, geom, nodata_value):
+    
+    # Create empty list to hold clipped arrays
+    clipped_arrs = []
+    
+    # Iterate over rasters
+    for file in raster_pathlist:
+
+        # Read raster
+        with rasterio.open(file) as rast:
+            
+            # Only process the first three bands (RGB)
+            indices = [1,2,3]
+            
+            # Mask pixels outside aoi with NoData values
+            raster_clip, out_transform = mask(rast, geom, crop = True, 
+                                              nodata = nodata_value, 
+                                              indexes = indices)
+        
+            # Copy metadata
+            out_meta = rast.meta.copy()
+            
+            # Update metadata
+            out_meta.update({
+                'driver': 'GTiff',
+                'dtype': 'uint8',
+                'count': len(indices),
+                'height': raster_clip.shape[1],
+                'width': raster_clip.shape[2],
+                'transform': out_transform,
+                'nodata': nodata_value})
+        
+        clipped_arrs.append(raster_clip)
+        
+    return clipped_arrs, out_meta
+
+# Define function to plot frames based on point
+def pnt_plot(raster_pathlist, val_frames, pntindex, pxsize_m):
+    
+    # Extract relevant point
+    point = val_frames["geometry"][pntindex]
+    
+    # Extract relevant frame
+    frame = [val_frames["frame"][pntindex]]
+    
+    # Clip validation data to frame
+    clipped_arrs, meta = clip_raster(raster_pathlist, frame, nodata_val)
+    
+    # Transpose clipped array to match format for imshow
+    clipped_rgb_arrs = [arr.transpose(1, 2, 0) for arr in clipped_arrs]
+    
+    # Extract transform data
+    transform = meta['transform']
+    
+    # Convert xy coordinate to image coordinates
+    px, py = ~transform * (point.x, point.y)
+    
+    # Create box in img pixel sizes
+    pxsize_px = pxsize_m / abs(transform.a) 
+    
+    # Define labels for subplots
+    labels = list(years)
+    
+    # Initialize figure with 3x4 subplots
+    fig, axs = plt.subplots(3, 4, figsize=(12, 9))
+    
+    # Flatten axes array
+    axs = axs.flatten()
+    
+    # Iterate over the arrays and corresponding axes
+    for i, rgb_array in enumerate(clipped_rgb_arrs):
+        
+        # Display rgb image
+        axs[i].imshow(rgb_array)
+        
+        # Remove axis labels
+        axs[i].axis('off')
+        
+        # Set subplot titles
+        axs[i].set_title(f'{labels[i]}')
+        
+        # Create pixel rectangle
+        rect = Rectangle(
+            
+            # Define bottom left corner
+            (px - pxsize_px / 2, py - pxsize_px / 2),
+            
+            # Define width and height in pixels
+            pxsize_px, pxsize_px, linewidth=1, edgecolor='red', facecolor='none'
+        )
+        
+        # Overlay validation pixel area
+        axs[i].add_patch(rect)
+        
+        # Remove empty subplot axes
+        for j in range(len(clipped_rgb_arrs), len(axs)):
+            axs[j].axis('off')
+
+    # Show plot
+    plt.tight_layout()
+    plt.show()
+
+# Create frames for each validation point
+val_frames = point_frame(valpoints, 1000)
+
+# Plot for defined point
+pnt_plot(planet_paths, val_frames, 3, 30)
+
+
+
