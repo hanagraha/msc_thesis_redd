9295bce9c3e2b8a0fda0e46432426072fcc39ff0
Hannah Graham <hannahikkai.graham@wur.nl>
2024-11-14 18:48:01 +0100

    Plotting stehman cm and stats

--- scripts/10_RQ2a_ValidationStatistics.py
+++ scripts/10_RQ2a_ValidationStatistics.py
@@ -16,9 +16,10 @@ Created on Thu Nov 14 10:52:36 2024
 import os
 import pandas as pd
 import geopandas as gpd
-from sklearn.metrics import confusion_matrix, accuracy_score, precision_score, recall_score, f1_score
+from sklearn.metrics import confusion_matrix, multilabel_confusion_matrix
 import seaborn as sns
 import matplotlib.pyplot as plt
+import numpy as np
 
 
 
@@ -47,6 +48,12 @@ out_dir = os.path.join(os.getcwd(), 'data', 'intermediate')
 # Set year range
 years = range(2013, 2024)
 
+# Set default plotting colors
+defaultblue = "#4682B4"
+reddcol = "brown"
+nonreddcol = "dodgerblue"
+grnpcol = "darkgreen"
+
 
 
 ############################################################################
@@ -56,7 +63,7 @@ years = range(2013, 2024)
 
 
 ############################################################################
-# Define filepath for validation data
+# Read validation data
 val_data = pd.read_csv("data/validation/validation_points_labelled.csv", delimiter=";")
 
 # Convert csv geometry to WKT
@@ -65,6 +72,19 @@ val_data['geometry'] = gpd.GeoSeries.from_wkt(val_data['geometry'])
 # Convert dataframe to geodataframe
 val_data = gpd.GeoDataFrame(val_data, geometry='geometry', crs="EPSG:32629") 
 
+# Read gfc stehman statistic data (calculated in R)
+gfc_stehman = pd.read_csv("data/validation/gfc_stehmanstats.csv", delimiter=",")
+gfc_stehman_cm = pd.read_csv("data/validation/gfc_confmatrix.csv", delimiter=",")
+
+# Read tmf stehman statistic data (calculated in R)
+tmf_stehman = pd.read_csv("data/validation/tmf_stehmanstats.csv", delimiter=",")
+tmf_stehman_cm = pd.read_csv("data/validation/tmf_confmatrix.csv", delimiter=",")
+
+# Read se stehman statistic data (calculated in R)
+se_stehman = pd.read_csv("data/validation/se_stehmanstats.csv", delimiter=",")
+se_stehman_cm = pd.read_csv("data/validation/se_confmatrix.csv", delimiter=",")
+
+datanames = ["GFC", "TMF", "Sensitive Early"]
 
 
 ############################################################################
@@ -74,16 +94,36 @@ val_data = gpd.GeoDataFrame(val_data, geometry='geometry', crs="EPSG:32629")
 
 
 ############################################################################
-# Select the two columns you want to analyze
+# Define dataset labels
+yearlabs = [0] + list(years)
+
+# Define function to plot three confusion matrices side by side
+def matrix_plt(matrices, names, fmt):
+    
+    # Initiate figure and subplots
+    fig, axes = plt.subplots(1, 3, figsize=(18, 5))
+    
+    # Iterate over each confusion matrix
+    for i in range(0, len(matrices)):
+        
+        # Create heatmap
+        sns.heatmap(matrices[i], annot=True, fmt=fmt, cmap='Blues', ax=axes[i],
+                    xticklabels=yearlabs, yticklabels=yearlabs)
+        
+        # Add title
+        axes[i].set_title(f'{names[i]} Confusion Matrix')
+        axes[i].set_xlabel(f'{names[i]} Predicted Labels')
+        axes[i].set_ylabel('Validation Labels')
+    
+    # Adjust layout
+    plt.tight_layout()
+    plt.show()
+
+# Extract data from validation dataset
 gfc = val_data['gfc']
 tmf = val_data['tmf']
 se = val_data['se']
 val_first = val_data['defor1']
-
-# Define dataset labels
-yearlabs = [0] + list(years)
-
-from sklearn.metrics import multilabel_confusion_matrix
 
 # Calculate confusion matrix for gfc
 gfc_matrix = confusion_matrix(val_first, gfc)
@@ -94,33 +134,8 @@ tmf_matrix = confusion_matrix(val_first, tmf)
 # Calculate confusion matrix for sensitive early combination
 se_matrix = confusion_matrix(val_first, se)
 
-# Define figure and subplots
-fig, axes = plt.subplots(1, 3, figsize=(18, 5))
-
-# Plot GFC confusion matrix
-sns.heatmap(gfc_matrix, annot=True, fmt='d', cmap='Blues', ax=axes[0],
-            xticklabels=yearlabs, yticklabels=yearlabs)
-axes[0].set_title('GFC Confusion Matrix')
-axes[0].set_xlabel('GFC Predicted Labels')
-axes[0].set_ylabel('Validation Labels')
-
-# Plot TMF confusion matrix
-sns.heatmap(tmf_matrix, annot=True, fmt='d', cmap='Blues', ax=axes[1],
-            xticklabels=yearlabs, yticklabels=yearlabs)
-axes[1].set_title('TMF Confusion Matrix')
-axes[1].set_xlabel('TMF Predicted Labels')
-axes[1].set_ylabel('Validation Labels')
-
-# Plot Sensitive Early Combination confusion matrix
-sns.heatmap(se_matrix, annot=True, fmt='d', cmap='Blues', ax=axes[2],
-            xticklabels=yearlabs, yticklabels=yearlabs)
-axes[2].set_title('Sensitive Early Combination Confusion Matrix')
-axes[2].set_xlabel('Sensitive Early Predicted Labels')
-axes[2].set_ylabel('Validation Labels')
-
-# Adjust layout
-plt.tight_layout()
-plt.show()
+# Plot calculated matrices
+matrix_plt([gfc_matrix, tmf_matrix, se_matrix], datanames, 'd')
 
 
 
@@ -175,36 +190,100 @@ mlmatrix_plt(se_mlmatrix, "Sensitive Early")
 ############################################################################
 
 
-# CALCULATE STATISTICS
+# MANIPULATE STEHMAN CONFUSION MATRICES
 
 
 ############################################################################
-# Define function to calculate group of statistics
-def statlist(true, pred):
+# Define function to formate stehman confusion matrix
+def steh_cm(stehman_cm, deci):
     
-    # Calculate accuracy
-    acc = accuracy_score(true, pred)
+    # Exclude the sum row and indices
+    cm = stehman_cm.iloc[:-1, 1:-1]
     
-    # Calculate precision
+    # Fill na values with 0
+    cm = cm.fillna(0)
     
-# Calculate metrics
-accuracy = accuracy_score(y_true, y_pred)
-precision = precision_score(y_true, y_pred, average='macro')  # use 'macro' for multiclass
-recall = recall_score(y_true, y_pred, average='binary')        # use 'macro' for multiclass
-f1 = f1_score(y_true, y_pred, average='binary')                # use 'macro' for multiclass
+    # Convert dataframe to array
+    cm = cm.to_numpy()
+    
+    # Round to (deci) number of decimals
+    cm = np.round(cm, deci)
+    
+    return cm
 
-# Print results
-print("Confusion Matrix:\n", conf_matrix)
-print("Accuracy:", accuracy)
-print("Precision:", precision)
-print("Recall:", recall)
-print("F1 Score:", f1)
+# Format stehman's gfc confusion matrix
+gfc_scm = steh_cm(gfc_stehman_cm, 3)
+
+# Format stehman's tmf confusion matrix
+tmf_scm = steh_cm(tmf_stehman_cm, 3)
+
+# Format stehman's se confusion matrix
+se_scm = steh_cm(se_stehman_cm, 3)
+
+# Plot confusion matrices
+matrix_plt([gfc_scm, tmf_scm, se_scm], datanames, '.2f')
 
 
 
+############################################################################
 
 
+# MANIPULATE STEHMAN STATISTICS
 
+
+############################################################################
+# Define function to plot stehman stats
+def steh_lineplt(datalist, stat, se, ylab):
+    
+    # Initialize plot
+    plt.figure(figsize=(10, 6))
+    
+    # Iterate over each dataset
+    for data, name in zip(datalist, datanames):
+        
+        # Add lines with error bars
+        plt.errorbar(data['year'], data[stat], yerr=data[se], fmt='-o', 
+                     color=defaultblue, capsize=5, elinewidth=1, ecolor='r', 
+                     label=name)
+    
+    # Add gridlines
+    plt.grid(True, linestyle = "--")
+    
+    # Add tickmarks
+    plt.xticks(years)
+    
+    # Add labels and title
+    plt.xlabel('Year')
+    plt.ylabel(ylab)
+    plt.title(f"{ylab} for GFC, TMF, and Sensitive Early Datasets")
+    
+    # Show legend
+    plt.legend()
+
+    # Display the plot
+    plt.tight_layout()
+    plt.show()
+
+# Remove gfc statistics for year 0
+gfc_stehman0 = gfc_stehman[1:]
+
+# Remove tmf statistics for year 0
+tmf_stehman0 = tmf_stehman[1:]
+
+# Remove se statistics for year 0
+se_stehman0 = se_stehman[1:]
+
+# Plot user's accuracy
+steh_lineplt([gfc_stehman0, tmf_stehman0, se_stehman0], 'ua', 'se_ua', 
+             "User's Accuracy")
+
+# Plot producer's accuracy
+steh_lineplt([gfc_stehman0, tmf_stehman0, se_stehman0], 'pa', 'se_pa', 
+             "Producer's Accuracy")
+
+# Plot area estimates
+steh_lineplt([gfc_stehman0, tmf_stehman0, se_stehman0], 'area', 'se_a', 
+             "Deforestation Area Estimates")
 
 
 
