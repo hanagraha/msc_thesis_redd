5cdeac81332c19c848a37c769558bdc2507209ec
hanagraha <hanagraha@gmail.com>
2026-02-02 20:57:05 +0100

    stehman statistics for yearmatching + buffer

--- scripts/a3_native_stehman.R
+++ scripts/a3_native_stehman.R
@@ -23,8 +23,8 @@ stratmap <- rast("validation/stratification_maps/stratification_layer_nogrnp.tif
 # -------------------------------------------------------------------------
 # DEFINE KEY FUNCTIONS
 # -------------------------------------------------------------------------
-# Define function to calculate time insensitive statistics
-timeinsensitive <- function(valdata, stratmap, filename){
+# Define function to calculate stehman statistics
+valstats <- function(valdata, stratmap, folder, filename){
   
   # Calculate number of pixels per strata
   pixel_counts <- freq(stratmap, digits = 0)
@@ -53,20 +53,19 @@ timeinsensitive <- function(valdata, stratmap, filename){
   
   # Export annual data
   write.csv(stats_df, 
-            file = sprintf("native_validation/timeinsensitive/%s_stats.csv", 
-                           filename), row.names = FALSE)
+            file = sprintf("native_validation/%s/%s_stats.csv", 
+                           folder, filename), row.names = FALSE)
   
   # Export confusion matrix
   write.csv(stats$matrix, 
-            file = sprintf("native_validation/timeinsensitive/%s_cm.csv", 
-                           filename))
+            file = sprintf("native_validation/%s/%s_cm.csv", 
+                           folder, filename))
   
   # Print statement
   print("Stehman statistics calculated and saved to file")
   
   return(stats)
 }
-
 
 
 # -------------------------------------------------------------------------
@@ -80,14 +79,31 @@ tmf_optA_dist_buff <- read.csv("native_validation/timeinsensitive/tmf_timeinsens
 tmf_optA_defor_buff <- read.csv("native_validation/timeinsensitive/tmf_timeinsensitive_dist_buffered.csv")
 
 # Calculate statistics
-gfc_optA_stats <- timeinsensitive(gfc_optA, stratmap, "gfc_timeinsensitive")
-tmf_optA_stats <- timeinsensitive(tmf_optA, stratmap, "tmf_timeinsensitive")
-gfc_optA_buff_stats <- timeinsensitive(gfc_optA_buff, stratmap, "gfc_timeinsensitive_buffered")
-tmf_optA_distbuff_stats <- timeinsensitive(tmf_optA_dist_buff, stratmap, "tmf_timeinsensitive_dist_buffered")
-tmf_optA_deforbuff_stats <- timeinsensitive(tmf_optA_defor_buff, stratmap, "tmf_timeinsensitive_defor_buffered")
+gfc_optA_stats <- timeinsensitive(gfc_optA, stratmap, "timeinsensitive", 
+                      "gfc_timeinsensitive")
+tmf_optA_stats <- timeinsensitive(tmf_optA, stratmap, "timeinsensitive", 
+                      "tmf_timeinsensitive")
+gfc_optA_buff_stats <- timeinsensitive(gfc_optA_buff, stratmap, "timeinsensitive", 
+                      "gfc_timeinsensitive_buffered")
+tmf_optA_distbuff_stats <- timeinsensitive(tmf_optA_dist_buff, stratmap, 
+                      "timeinsensitive","tmf_timeinsensitive_dist_buffered")
+tmf_optA_deforbuff_stats <- timeinsensitive(tmf_optA_defor_buff, stratmap, 
+                      "timeinsensitive","tmf_timeinsensitive_defor_buffered")
 
 
+# -------------------------------------------------------------------------
+# YEAR MATCH STATISTICS
+# -------------------------------------------------------------------------
+# Read map and reference labels
+gfc_optB_buff <- read.csv("native_validation/yearmatch/gfc_yearmatch_buffered.csv")
+tmf_optB_defor_buff <- read.csv("native_validation/yearmatch/tmf_yearmatch_defor_buffered.csv")
+tmf_optB_dist_buff <- read.csv("native_validation/yearmatch/tmf_yearmatch_dist_buffered.csv")
 
-
-
+# Calculate statistics
+gfc_optB_buff_stats <- valstats(gfc_optB_buff, stratmap, "yearmatch",
+                                "gfc_yearmatch_buffered")
+tmf_optB_deforbuff_stats <- valstats(tmf_optB_defor_buff, stratmap, 
+    "yearmatch", "tmf_yearmatch_defor_buffered")
+tmf_optB_distbuff_stats <- valstats(tmf_optB_dist_buff, stratmap, 
+    "yearmatch", "tmf_yearmatch_dist_buffered")
 
